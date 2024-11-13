e63c575c360cd2e9b72730d45b8c86cc448f5abc
Kauan Marques da Luz <daluzmarkes@gmail.com>
2024-11-13 16:02:04 -0400

    feat: update script

--- Prova_bd_3.sql
+++ Prova_bd_3.sql
@@ -2,46 +2,44 @@ create database Prova_BD_3;
 use Prova_BD_3;
 
 # === Livres ===
-create table Credencial(
-	id_cre int primary key not null,
-    nome_cre varchar(300) not null,
-    cpf_cre varchar(14) not null,
-    data_nasc_cre date,
-    telefone_cre varchar(11)
-);
-
 create table Produto(
-	id_pro int primary key not null,
-    descricao_pro varchar(300),
-    valor_pro float
+	id_pro int primary key auto_increment,
+    descricao_pro varchar(300) not null,
+    valor_pro float not null
 );
 
 # === Semi-Livres ===
 create table Funcionario(
-	id_fun int primary key not null,
-    salario_fun float not null,
-	
-    id_cre_fk int,
-    foreign key(id_cre_fk) references Credencial(id_cre)
+	id_fun int primary key auto_increment,
+	nome_usu varchar(300) not null,
+    cpf_usu varchar(50) not null,
+    email_usu varchar(100) not null,
+    rg_usu varchar(20) not null,
+    data_nasc_usu date not null,
+    salario_fun float not null
 );
 
 create table Cliente(
-	id_cli int primary key not null,
-	
-    id_cre_fk int,
-    foreign key(id_cre_fk) references Credencial(id_cre)
+	id_cli int primary key auto_increment,
+	nome_cli varchar(300) not null,
+    cpf_cli varchar(50) not null,
+    email_cli varchar(100) not null,
+    rg_cli varchar(20) not null,
+    data_nasc_cli date not null
 );
 
 create table Usuario(
-	id_usu int primary key not null,
-	
-    id_cre_fk int,
-    foreign key(id_cre_fk) references Credencial(id_cre)
+	id_usu int primary key auto_increment,
+	nome_usu varchar(300) not null,
+    cpf_usu varchar(50) not null,
+    email_usu varchar(100) not null,
+    rg_usu varchar(20) not null,
+    data_nasc_usu date not null
 );
 
 create table Venda(
-	id_ven int primary key not null,
-    valor_total_ven float,
+	id_ven int primary key auto_increment,
+    valor_total_ven float not null,
     data_ven date not null,
     hora_ven time not null,
     
@@ -51,24 +49,24 @@ create table Venda(
 
 # === Presas ===
 create table Estoque(
-	id_est int primary key not null,
+	id_est int primary key auto_increment,
     unidade_est varchar(100) not null,
-	quantidade_est int,
+	quantidade_est int not null,
     
     id_pro_fk int,
     foreign key(id_pro_fk) references Produto(id_pro)
 );
 
 create table Forma_Pagamento(
-	id_fpag int primary key not null,
-    forma_fpag varchar(200),
+	id_fpag int primary key auto_increment,
+    forma_fpag varchar(200) not null,
     
     id_ven_fk int,
     foreign key(id_ven_fk) references Venda(id_ven)
 );
 
 create table FunVen(
-	id_funven int primary key not null,
+	id_funven int primary key auto_increment,
     
     id_fun_fk int,
     id_ven_fk int,
@@ -77,16 +75,14 @@ create table FunVen(
 );
 
 create table CliVen(
-	id_cliven int primary key not null,
+	id_cliven int primary key auto_increment,
     
-    id_cli_fk int,
     id_ven_fk int,
-    foreign key(id_cli_fk) references Cliente(id_cli),
     foreign key(id_ven_fk) references Venda(id_ven)
 );
 
 create table UseVen(
-	id_useven int primary key not null,
+	id_useven int primary key auto_increment,
     
     id_usu_fk int,
     id_ven_fk int,
@@ -96,6 +92,27 @@ create table UseVen(
 
 # === Metodos ===
 
-delimiter $$ 
 
+# == Cadastrar Produto ==
+delimiter $$ 
+create procedure cadastroProduto(descricaoProd varchar(300), valorProd float)
+begin
+	if(descricaoProd is not null) then
+		if(valorProd is not null) then
+			if(valorProd <= 0 or valorProd >= 1000) then
+				select "O valor do produto não pode ser maior que R$ 1000,00 ou menor que R$ 0,00" as confirmacao;
+			else 
+				insert into Produto values (null, descricaoProd, valorProd);
+            end if;
+		else
+			select "Valor produto é inválido" as confimacao;
+        end if;	
+	else 
+		select "Descrição do produto é inválida" as confirmacao;
+    end if;
+end;
 $$ delimiter ;
+
+call cadastroProduto("", 590);
+
+select * from produto;
